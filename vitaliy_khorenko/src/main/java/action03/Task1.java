c218d1e524a1cda05e64ac9942a1335ce8280992
Vitaliy <Vitaliy@JavaVit>
2016-09-27 19:24:45 +0300

    full commit

---
+++ vitaliy_khorenko/src/main/java/action03/Task1.java
@@ -0,0 +1,43 @@
+package action03;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		
+		Map<Integer, String> map = new HashMap<>();
+		f(map);
+		
+		map.put(10, "v10");
+		map.put(30, "v30");
+		map.put(40, "v40");
+		map.put(20, "v20");
+		
+		Set<Integer> set1 = map.keySet();
+		Collection<String> values = map.values();
+		
+		Set<Entry<Integer, String>> set2 = map.entrySet();
+		for(Entry<Integer, String> entry : set2){
+			System.out.print(entry.getKey());
+			System.out.print(" - ");
+			System.out.println(entry.getValue());
+		}
+		
+		
+		
+		
+		
+
+	}
+
+	private static void f(Map<Integer, String> map) {
+		System.out.println(map.toString());
+		
+	}
+
+}
