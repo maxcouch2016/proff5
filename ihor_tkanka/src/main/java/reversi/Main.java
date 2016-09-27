4c6d26b0b9468be7aaa2279c2b391ec540ba4fac
Kayenushka <kayen@ukr.net>
2016-09-27 19:05:54 +0300

    Upd

--- ihor_tkanka/src/main/java/reversi/Main.java
+++ ihor_tkanka/src/main/java/reversi/Main.java
@@ -1,12 +1,32 @@
 package reversi;
 
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-public class Main  {
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+	
+			primaryStage.setTitle("JavaFX Welcome");
+			GridPane grid = new GridPane();
+		    grid.setAlignment(Pos.CENTER);
+		    grid.setHgap(10);
+		    grid.setVgap(10);
+		    grid.setPadding(new Insets(25, 25, 25, 25));
+		    Scene scene = new Scene(grid, 300, 275);
+		    primaryStage.setScene(scene);
+		    primaryStage.show();
+		    
+	}
 
 	public static void main(String[] args) {
-	
-		
-		
+		launch(args);
 	}
 }
