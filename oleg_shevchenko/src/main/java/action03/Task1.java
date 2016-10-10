76f0e2fc3a659650021a8627da4f257c58d7a102
olegs <oleg.sh.vi@gmail.com>
2016-10-10 19:17:30 +0300

    hashmap

---
+++ oleg_shevchenko/src/main/java/action03/Task1.java
@@ -0,0 +1,11 @@
+package action03;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task1 {
+	public static void main(String[] args) {
+		Map<Integer, String> map = new HashMap<>();
+	}
+	
+}
---
+++ oleg_shevchenko/src/main/java/action04/Task03.java
@@ -0,0 +1,38 @@
+package action04;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Task03 {
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<>(
+				Arrays.asList(
+						new Integer[]{10,20,80,40,15})
+				);
+
+		System.out.println("[");
+		list.forEach(new Consumer<Integer>(){
+			@Override
+			public void accept(Integer t) {
+				System.out.print(", "+t);				
+			}
+		});
+		System.out.println("]");
+		
+		System.out.println("[");
+		list.forEach(new MyConsumer());
+		System.out.println("]");
+		
+	}
+	static class MyConsumer 
+					implements Consumer<Integer>{
+		@Override
+		public void accept(Integer t) {
+			System.out.print(", "+t);				
+		}
+	}
+	
+}
