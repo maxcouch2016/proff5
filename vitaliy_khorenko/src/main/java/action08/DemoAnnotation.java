be72f9cf0970ccc70283f9efe78f388c2df5892b
VKhorenko <khorenko@list.ru>
2016-10-24 21:42:34 +0300

    Save and read Message from DB

---
+++ vitaliy_khorenko/src/main/java/action08/DemoAnnotation.java
@@ -0,0 +1,10 @@
+package action08;
+
+public class DemoAnnotation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ vitaliy_khorenko/src/main/java/action11/MessageDB/Message.java
@@ -0,0 +1,28 @@
+package action11.MessageDB;
+
+public class Message {
+	
+	String message;
+
+	public Message(){
+		
+	}
+	
+	public Message(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "Message [message=" + message + "]";
+	}
+	
+}
